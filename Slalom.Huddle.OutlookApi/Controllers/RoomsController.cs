f53d0cf7309b2838725c02b709b2c6e797eb5a31
Pavan Kanaparthy <v-pavaka@microsoft.com>
2015-12-23 14:56:09 -0800

    Added deployment to Azure

---
+++ Slalom.Huddle.OutlookApi/Controllers/RoomsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Slalom.Huddle.OutlookApi.Controllers
+{
+    public class RoomsController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Room1", "Room2" };
+        }        
+    }
+}
